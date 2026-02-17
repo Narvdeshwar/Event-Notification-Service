b8c4bc352f9ac26282ec4e532b3b7440f92b2170
narvdeshwar <sam.07soni@gmail.com>
2026-02-17 16:42:19 +0530

    added the retry inside the poller

--- internal/worker/poller.go
+++ internal/worker/poller.go
@@ -9,34 +9,35 @@ import (
 )
 
 type Poller struct {
-	repo store.NotificationRepository
-	jobQueue chan model.Notification
+	repo      store.NotificationRepository
+	jobQueue  chan model.Notification
 	batchSize int
-	interval time.Duration
+	interval  time.Duration
 }
 
-func (p *Poller) Start(ctx context.Context){
-	ticker:=time.NewTicker(p.interval)
+func (p *Poller) Start(ctx context.Context) {
+	ticker := time.NewTicker(p.interval)
 	defer ticker.Stop()
-	for{
-		select{
+	for {
+		select {
 		case <-ctx.Done():
 			return
 		case <-ticker.C:
+			p.repo.RecoverStuckJob(ctx, 5*time.Minute)
 			p.fetch()
 		}
 	}
 }
 
-func (p *Poller) fetch(){
-	ctx,cancel:=context.WithTimeout(context.Background(),2*time.Second)
+func (p *Poller) fetch() {
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
 	defer cancel()
-	jobs,err:=p.repo.fetchAndMarkProcessing(ctx,p.batchSize)
-	if err!=nil{
-		log.Println("fetch error",err)
+	jobs, err := p.repo.fetchAndMarkProcessing(ctx, p.batchSize)
+	if err != nil {
+		log.Println("fetch error", err)
 		return
 	}
-	for _,job:=range jobs{
-		p.jobQueue<-job
+	for _, job := range jobs {
+		p.jobQueue <- job
 	}
 }
