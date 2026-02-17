e13ffccad99113709947972d240577b9b12a64ec
narvdeshwar <sam.07soni@gmail.com>
2026-02-17 15:05:53 +0530

    added the db sent status

---
+++ internal/store/postgres_state.go
@@ -0,0 +1,12 @@
+package store
+
+import "context"
+
+func (r *PostGresRepo) MarkSent(ctx context.Context, id string) error {
+	query := `UPDATE notifications 
+	set STATUS='SENT', updated_at=now()
+	Where id=$1
+	`
+	_, err := r.db.ExecContext(ctx, query, id)
+	return err
+}
